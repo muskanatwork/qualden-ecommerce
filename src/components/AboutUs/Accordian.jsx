934d43f407584b698d6b636d449b99f23e6d82d7
Muskan Kumari <muskanatwork23@gmail.com>
2024-06-11 09:09:43 +0530

    create icons on accordian

--- src/components/AboutUs/Accordian.jsx
+++ src/components/AboutUs/Accordian.jsx
@@ -7,21 +7,44 @@ const Accordion = () => {
         setOpenAccordion(openAccordion === index ? null : index);
     };
 
+    const products = [
+        "Electronic Sensor",
+        "Radar Sensor",
+        "Tipping Bucket Rain Gauge",
+        "3D Ultrasonic Anemometer",
+        "SMC BRNO-ECO Optical Rain Gauge",
+        "GPS Receiver Rs232",
+        "D116 Ultrasonic Flow Meter",
+        "SMKB MET ROAD FS1 Weather Station",
+        "SMKB MET ROAD FS2 Weather Station",
+        "KB -Tab100 Industrial Pc"
+    ];
+
+    const productDetails = [
+        "Details about Electronic Sensor",
+        "Details about Radar Sensor",
+        "Details about Tipping Bucket Rain Gauge",
+        "Details about 3D Ultrasonic Anemometer",
+        "Details about SMC BRNO-ECO Optical Rain Gauge",
+        "Details about GPS Receiver Rs232",
+        "Details about D116 Ultrasonic Flow Meter",
+        "Details about SMKB MET ROAD FS1 Weather Station",
+        "Details about SMKB MET ROAD FS2 Weather Station",
+        "Details about KB -Tab100 Industrial Pc"
+    ];
+
     return (
         <div className='accordianDiv'>
             <h3>Products</h3>
-            <hr/>
-            <div>Electronic Sensor </div>
-            <div>Radar Sensor </div>
-            <div>Tipping Bucket Rain Gauge</div>
-            <div>3D Ultrasonic Anemometer </div>
-            <div>SMC BRNO-ECO Optical Rain Gauge </div>
-            <div> GPS Receiver Rs232</div>
-            <div> D116 Ultrasonic Flow Meter</div>
-            <div>SMKB MET ROAD FS1 Weather Station </div>
-            <div> SMKB MET ROAD FS2 Weather Station</div>
-            <div>KB -Tab100 Industrial Pc </div>
-            <div>mujhe eska accordian bna ke dijiye</div>
+            <hr />
+            {products.map((product, index) => (
+                <div key={index} className='accordianItem'>
+                    <div onClick={() => handleAccordion(index)} className='accordianHeader'>
+                        {product} <span className='accordianIcon'>{openAccordion === index ? '-' : '+'}</span>
+                    </div>
+                    {openAccordion === index && <p className='accordianContent'>{productDetails[index]}</p>}
+                </div>
+            ))}
         </div>
     );
 };

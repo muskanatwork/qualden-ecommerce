832811479cfa02d85cd549038d11a34f5e4b3417
Muskan Kumari <muskanatwork23@gmail.com>
2024-06-12 10:04:12 +0530

    create naveInfo icon

--- src/components/Common/NavInfo.jsx
+++ src/components/Common/NavInfo.jsx
@@ -1,19 +1,20 @@
+import { FaEnvelope, FaMobile } from 'react-icons/fa'; 
+
 const NavInfo = () => {
     return (
         <div className="Nav-info">
             <div className="info-heading">
-                <h3>qualden@gmail.com</h3>
-                <h3>08069247354</h3>
+                <h3><FaEnvelope /> qualden@gmail.com</h3>
+                <h3><FaMobile /> 08069247354</h3>
                 <h3>Ext : 036</h3>
             </div>
             <div className="info-heading">
                 <h3>GST No. 05AWBPA1798G1ZS</h3>
-                <button className="info-button email-button">Send email</button>
+                <button className="info-button email-button"><FaEnvelope /> Send email</button>
                 <button className="info-button sms-button">Send SMS</button>
             </div>
         </div>
     );
-
 }
 
-export default NavInfo
+export default NavInfo;

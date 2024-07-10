bec3b4c98659668ebb3a51974356ac0562e3e5e9
Muskan Kumari <muskanatwork23@gmail.com>
2024-07-10 12:00:24 +0530

    remove Current jobs page

--- src/components/Common/FooterNav.jsx
+++ src/components/Common/FooterNav.jsx
@@ -10,7 +10,6 @@ const FooterNav = () => {
                         <li><Link to="/about">About Us</Link></li>
                         <li><Link to="/products">Products</Link></li>
                         <li><Link to="/contact">Contact Us</Link></li>
-                        <li><Link to="/currentJobs">Current Jobs</Link></li>
                     </ul>
                 <div className="horiLine">
                     <div></div>

c85f7c12da0ea0b75fe6d097e20456e2844d7aa2
Muskan Kumari <muskanatwork23@gmail.com>
2024-07-10 10:19:20 +0530

    add div on nav

--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -4,6 +4,7 @@ import Search from '../Common/SearchBar';
 import Data from "../Products/Data.json";
 import PhoneSearch from '../Common/PhoneSearch';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
@@ -49,67 +50,69 @@ function Nav() {
   };
 
   return (
-    <nav ref={navRef} className={`${scrolled ? 'scrolled' : ''}`}>
-      <Link to='/'>
-        <img className='logo' src="https://qualden.com/assets/imgs/template/logo.png" alt="qualden" />
-      </Link>
-      <div className='nav-right'>
-        <ul className={menuOpen ? 'open' : ''}>
-          {menuOpen && (
+    <div className={`nav-container ${scrolled ? 'scrolled' : ''}`}>
+      <nav ref={navRef}>
+        <Link to='/'>
+          <img className='logo' src="https://qualden.com/assets/imgs/template/logo.png" alt="qualden" />
+        </Link>
+        <div className='nav-right'>
+          <ul className={menuOpen ? 'open' : ''}>
+            {menuOpen && (
+              <li>
+                <span className='close-menu' onClick={() => setMenuOpen(false)}>
+                  &times;
+                </span>
+              </li>
+            )}
             <li>
-              <span className='close-menu' onClick={() => setMenuOpen(false)}>
-                &times;
-              </span>
+              <NavLink to='/'>Home</NavLink>
             </li>
-          )}
-          <li>
-            <NavLink to='/'>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to='/about'>About Us</NavLink>
-          </li>
-          <li className='dropdown'>
-            <NavLink to='/product'>Products <ExpandMoreIcon/> </NavLink>
-            <div className="dropdown-content">
-              {Data.map((category, index) => (
-                Object.keys(category).map(catName => (
-                  <div
-                    key={catName}
-                    className='category'
-                    onMouseEnter={() => handleMouseEnter(catName)}
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {catName}
-                    {hoveredCategory === catName && (
-                      <div className='product-names'>
-                        {category[catName].slice(0, viewAll[catName] ? category[catName].length : 5).map((product, productIndex) => (
-                          <Link key={productIndex} to={`/product/${catName}/${product.productName}`}>
-                            {product.productName}
-                          </Link>
-                        ))}
-                        {catName === "Electronic-Sensor" && category[catName].length > 5 && !viewAll[catName] && (
-                          <button onClick={() => handleViewAll(catName)}>View All</button>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                ))
-              ))}
-            </div>
-          </li>
-          <li>
-            <NavLink to='/contact'>Contact Us</NavLink>
-          </li>
-        </ul>
-        <Search />
-        <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
-          <span></span>
-          <span></span>
-          <span></span>
+            <li>
+              <NavLink to='/about'>About Us</NavLink>
+            </li>
+            <li className='dropdown'>
+              <NavLink to='/product'>Products <ExpandMoreIcon/> </NavLink>
+              <div className="dropdown-content">
+                {Data.map((category, index) => (
+                  Object.keys(category).map(catName => (
+                    <div
+                      key={catName}
+                      className='category'
+                      onMouseEnter={() => handleMouseEnter(catName)}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      {catName}
+                      {hoveredCategory === catName && (
+                        <div className='product-names'>
+                          {category[catName].slice(0, viewAll[catName] ? category[catName].length : 5).map((product, productIndex) => (
+                            <Link key={productIndex} to={`/product/${catName}/${product.productName}`}>
+                              {product.productName}
+                            </Link>
+                          ))}
+                          {catName === "Electronic-Sensor" && category[catName].length > 5 && !viewAll[catName] && (
+                            <button className='viewAllButton' onClick={() => handleViewAll(catName)}>View All</button>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  ))
+                ))}
+              </div>
+            </li>
+            <li>
+              <NavLink to='/contact'>Contact Us</NavLink>
+            </li>
+          </ul>
+          <Search />
+          <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-      </div>
-      <PhoneSearch />
-    </nav>
+        <PhoneSearch />
+      </nav>
+    </div>
   );
 }
 
